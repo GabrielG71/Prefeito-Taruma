c5cff980a468107f52be8f40592074c017f3b787
GabrielG71 <gabrielgoncalves2981@gmail.com>
2025-02-15 01:07:55 -0300

    whatfuck is happening

--- menu.php
+++ menu.php
@@ -1,24 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu</title>
     <link rel="stylesheet" href="menu.css">
-    <script defer src="app_menu.js"></script>
 </head>
 <body>
-    <header class="header">
-        <nav class="nav">
-            <div class="prefeitura">
-                <a href="#" id="logo_prefeitura"><li><img src="Imagens/logo-tarumã.png" alt=""></li></a>
-                <a href="#" id="marca_prefeitura"><li><img src="Imagens/marca_prefeitura.png" alt=""></li></a>
-            </div>
-            <button class="hamburguer"></button>
-            <ul class="nav-list">
-                <li><a href="index.html">DESLOGAR</a></li>
-            </ul>   
-        </nav>
+    <header>
+        <h1>PAU</h1>
     </header>
 </body>
 </html>
