ae9601e610b0f2847a25f66cebb7c63fac954ff0
GabrielG71 <gabrielgoncalves2981@gmail.com>
2025-02-15 02:20:36 -0300

    att menu

--- menu.php
+++ menu.php
@@ -3,12 +3,60 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu</title>
+    <title>Prefeitura Tarumã</title>
     <link rel="stylesheet" href="menu.css">
+    <script defer src="app_menu.js"></script>
 </head>
 <body>
-    <header>
-        <h1>PAU</h1>
+    <header class="header">
+        <nav class="nav">
+            <div class="prefeitura">
+                <a href="#" id="logo_prefeitura"><li><img src="Imagens/logo-tarumã.png" alt=""></li></a>
+                <a href="#" id="marca_prefeitura"><li><img src="Imagens/marca_prefeitura.png" alt=""></li></a>
+            </div>
+            <button class="hamburguer"></button>
+            <ul class="nav-list">
+                <li><a href="login.php">DESLOGAR</a></li>
+            </ul>   
+        </nav>
     </header>
+    <main class="main-content">
+    <h2>Horários dos Ônibus</h2>
+    <h3>Aluno: Gabriel Gonçalves</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>Local</th>
+                    <th>Horário de Ida</th>
+                    <th>Horário de Volta</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Centro</td>
+                    <td>06:00</td>
+                    <td>18:00</td>
+                </tr>
+                <tr>
+                    <td>Bairro A</td>
+                    <td>07:30</td>
+                    <td>19:30</td>
+                </tr>
+                <tr>
+                    <td>Bairro B</td>
+                    <td>09:00</td>
+                    <td>21:00</td>
+                </tr>
+                <tr>
+                    <td>Universidade</td>
+                    <td>10:45</td>
+                    <td>22:15</td>
+                </tr>
+            </tbody>
+        </table>
+    </main>    
+    <footer class="footer">
+        <p>&copy; 2024 Prefeitura de Tarumã. Todos os direitos reservados.</p>
+    </footer>
 </body>
 </html>
