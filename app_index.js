68bb7bb403486bf4cacc1287377430836569667e
DanZx819 <147755543+DanZx819@users.noreply.github.com>
2025-02-15 23:41:15 -0300

    Criação do slider e correção do menu hamburguer

--- app_index.js
+++ app_index.js
@@ -1,7 +1,27 @@
-const hamburguer = document.querySelector(".hamburguer");
-const nav = document.querySelector(".nav");
+const hamburguer = document.querySelector('.hamburguer');
+const nav = document.querySelector('.container-box');
 const navLinks = document.querySelectorAll('.nav-list li a');
 
 hamburguer.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
+
+var radio = document.querySelector('.manual-btn')
+
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+  proximaImg()
+}, 5000)
+
+function proximaImg(){
+    cont++
+    if (cont > 3) {
+      cont = 1
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
