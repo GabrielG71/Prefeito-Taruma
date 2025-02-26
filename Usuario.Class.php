94a5c0ad02398eb1519d58610e057c9baf15c3c6
GabrielG71 <gabrielgoncalves2981@gmail.com>
2025-02-25 22:28:44 -0300

    Change ip

--- Usuario.Class.php
+++ Usuario.Class.php
@@ -4,7 +4,6 @@ class Usuario {
     public function login($cpf, $senha) {
         global $pdo;
 
-        // Buscar apenas a senha hash armazenada no banco
         $sql = "SELECT iduser, admin, pass FROM usuarios WHERE cpf = :cpf";
         $sql = $pdo->prepare($sql);
         $sql->bindValue(':cpf', $cpf);
@@ -13,7 +12,6 @@ class Usuario {
         if ($sql->rowCount() > 0) {
             $dado = $sql->fetch();
 
-            // Verifica a senha digitada com a senha armazenada no banco
             if (password_verify($senha, $dado['pass'])) {
                 $_SESSION['idu'] = $dado['iduser'];
                 $_SESSION['admin'] = $dado['admin'];
