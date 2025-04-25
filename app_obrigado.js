7e9a6cb5b7d10047549b64faef3a5cacc8039c0f
Daniel de Oliveira Colonheze <danielcolonhze@gmail.com>
2025-04-25 19:39:30 -0300

    app_obrigado.js

--- app_obrigado.js
+++ app_obrigado.js
@@ -1,7 +1,13 @@
+// Pega o botão do menu
 const hamburguer = document.querySelector(".hamburguer");
+
+// Pega o menu de navegação
 const nav = document.querySelector(".nav");
+
+// Pega os links do menu
 const navLinks = document.querySelectorAll('.nav-list li a');
 
+// Quando clicar no botão, ativa ou desativa o menu
 hamburguer.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
