d57d64a1d382777dc0ee47cb888575f5e4c78acc
Daniel de Oliveira Colonheze <danielcolonhze@gmail.com>
2025-04-25 20:34:15 -0300

    logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,13 @@
-<?php 
+<?php
+// Inicia ou resume a sessão atual
 session_start();
+
+// Remove a variável de sessão 'idu' que armazena o ID do usuário logado
 unset($_SESSION['idu']);
 
+// Redireciona o usuário para a página inicial (index.html)
 header("Location: index.html");
 
+// Encerra a execução do script imediatamente após o redirecionamento
+exit;
 ?>
